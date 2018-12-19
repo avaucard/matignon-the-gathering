b6d78d03c760980136180d8953965131bffaa756
samy <samy_leni@hotmail.fr>
2018-12-19 17:54:49 +0100

    form css

--- src/Form/SignUpFormType.php
+++ src/Form/SignUpFormType.php
@@ -13,11 +13,11 @@ class SignUpFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('surname')
-            ->add('firstname')
-            ->add('email')
-            ->add('password', PasswordType::class)
-            ->add('confirmpassword', PasswordType::class)
+            ->add('surname', null, array('label' => false))
+            ->add('firstname', null, array('label' => false))
+            ->add('email', null, array('label' => false))
+            ->add('password', null, array('label' => false), PasswordType::class)
+            ->add('confirmpassword', null, array('label' => false), PasswordType::class)
             // ->add('ballotsNumber')
             // ->add('lastClaim')
             // ->add('isAdmin')
